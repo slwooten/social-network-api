75ab14c28b6b179d10a82128d1408869af2c9fe9
Seth Wooten <slwooten14@gmail.com>
2022-04-20 18:26:06 -0400

    completed all user routes

--- main/controllers/userController.js
+++ main/controllers/userController.js
@@ -10,5 +10,31 @@ module.exports = {
     User.create(req.body)
       .then((userData) => res.json(userData))
       .catch((err) => res.status(500).json(err));
-  }
+  },
+  getSingleUser(req, res) {
+    User.findOne({ _id: req.params.userId })
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: 'No user found with that ID' })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+  updateUser(req, res) {
+    User.updateOne(
+      {
+        "userId": req.params.userId
+      },
+      {
+        $set: { "username": req.body.username }
+      }
+    )
+      .then((user) => res.json(user))
+      .catch((err) => res.status(500).json(err));
+  },
+  deleteUser(req, res) {
+    User.deleteOne({ 'userId': req.params.userId })
+      .then(() => res.json({ message: 'User deleted.' }))
+      .catch((err) => res.status(500).json(err));
+  },
 }
--- main/routes/api/userRoutes.js
+++ main/routes/api/userRoutes.js
@@ -3,9 +3,15 @@ const router = require('express').Router();
 const {
   getUsers,
   createUser,
+  getSingleUser,
+  updateUser,
+  deleteUser
 } = require('../../controllers/userController');
 
 // get and create users
 router.route('/').get(getUsers).post(createUser);
+
+// get single user and update user by ID
+router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
 
 module.exports = router;
